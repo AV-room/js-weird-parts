09579617637c7a3b00a6d2a91d659c371131556f
AV-room <AVroom.github@gmail.com>
2017-08-20 18:53:02 +1000

    added comments

--- Greetr.js
+++ Greetr.js
@@ -9,7 +9,7 @@
         
         }
 
-        //private variables (e.g. can't be accessed by Greetr.supportedLangs)
+        //private variables; hidden within the scope of this IIFE (e.g. can't be accessed by Greetr.supportedLangs)
         //accessible to created Greetr function by closure -- 2:30
         var supportedLangs = ['en', 'es'];
 
@@ -31,14 +31,17 @@
         //this is where we'll put methods that we'll want to use inside the object returned from Greetr
         //this is a more efficient way to set up functionality across all Greetr instances
         //  since they all share this prototype. We could define the methods inside Greetr.init but
-        //  that means each instance of Greetr would take memory space to hold the same definition.    
+        //  that means each instance of Greetr would take memory space to hold the same definitions.    
         //we want this to be the prototype for all objects created from the Greetr.init function
         Greetr.prototype = {
             fullName: function(){
+                
+                //'this' refers to the calling object at execution time
                 return this.firstName + ' ' + this.lastName;
             },
 
             //throw error if given language is not supported by our function
+            //references the externally inaccessible 'supportedLangs' within the closure
             validate: function(){
                 if(supportedLangs.indexOf(this.language) === -1) {
                     throw 'Invalid language';
@@ -57,6 +60,7 @@
             greet: function(formal){
                 var msg;
 
+                //if undefined or null it will be coerced to 'false'
                 if(formal){
                     msg = this.formalGreeting();
                 }
@@ -114,6 +118,7 @@
                 }
 
                 //use jQuery to set the html on given selector
+                // i.e. inject the message in the chosen place in the DOM
                 $(selector).html(msg);
 
                 return this;
@@ -122,6 +127,7 @@
         };
 
         
+        // the actual object is created here, allowing us to 'new' an object without calling 'new'
         //Greetr.init builds each new function object instance
         //ok to create this after actual call as return statement above won't be called until Greetr is actually used
         //this Greetr.init code should have been run by then
@@ -139,7 +145,8 @@
         //configuring Greetr.init's prototype 
         Greetr.init.prototype = Greetr.prototype;
 
-        //set up the Greetr and G$ references to the Greetr function so we can use our library
+        //attach our Greetr to the global object, and provide a shorthand '$G' for ease our poor fingers
+        // i.e. set up the Greetr and G$ references to the Greetr function so we can use our library
         global.Greetr = global.G$ = Greetr;
 
     }(window, jQuery)
--- app.js
+++ app.js
@@ -1,16 +1,19 @@
+
+// gets a new object (our Greetr architecture allows us not to have to use the 'new' keyword here)
 var g = Greetr('Milly', 'Everett');
 console.log(g);
 
 var g2 = G$('Hugh', 'Banks');
 console.log(g2);
 
+//using our chainable methods
 g.greet().setLang('es').greet(true).log().HTMLGreeting(greeting, true);
 
 //using our framework with jQuery
 //on login click,
 $('#login').click(function(){
 
-    //create new greetr function and use it to set the content of our h1 according to selected language
+    //create new greetr function (with hardcoded name) and use it to set the content of our h1 according to selected language
     var loginGreetr = G$('Julia', 'Roberts');
     loginGreetr.setLang($('#lang').val()).HTMLGreeting('#greeting', true);
 
